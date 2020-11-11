da8672be126d573367196eb21414b18a2d8a8ac0
jamie <jamielepito@gmail.com>
2020-11-10 21:42:02 -0600

    Build recipe mapper and recipe repoitory

--- server/src/main/java/learn/collaboreat/data/RecipeJDBCTemplateRepository.java
+++ server/src/main/java/learn/collaboreat/data/RecipeJDBCTemplateRepository.java
@@ -1,4 +1,128 @@
 package learn.collaboreat.data;
 
+import learn.collaboreat.data.mappers.HealthInfoMapper;
+import learn.collaboreat.data.mappers.RecipeMapper;
+import learn.collaboreat.models.HealthInfo;
+import learn.collaboreat.models.HealthInfoRecipe;
+import learn.collaboreat.models.Recipe;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
+@Repository
 public class RecipeJDBCTemplateRepository {
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public RecipeJDBCTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public List<Recipe> findAll() {
+        final String sql =
+                "select recipeId, recipeName, recipeStory, recipeDescription, recipeIngredients, " +
+                        "recipeCookTime, recipeCookTime, recipeSteps, recipeDate, recipeRating, " +
+                        "userId, mealTypeId " +
+                        "from recipe;";
+        RecipeMapper recipeMapper = new RecipeMapper();
+        return jdbcTemplate.query(sql, recipeMapper);
+    }
+
+    public Recipe findById(int recipeId) {
+        final String recipeSql =
+                "select recipeId, recipeName, recipeStory, recipeDescription, recipeIngredients, " +
+                        "recipeCookTime, recipeCookTime, recipeSteps, recipeDate, recipeRating, " +
+                        "userId, mealTypeId " +
+                        "from recipe " +
+                        "where recipeId = ?;";
+
+        Recipe recipe = jdbcTemplate.query(recipeSql, new RecipeMapper(), recipeId).stream()
+                .findFirst().orElse(null);
+
+        // TODO: Add list of healthInfoRecipe with helper method
+        return recipe;
+    }
+
+    public Recipe add(Recipe recipe) {
+        final String sql = "insert into recipe " +
+                "(recipeId, recipeName, recipeStory, recipeDescription, recipeIngredients, recipeCookTime, " +
+                "recipeSteps, recipeDate, recipeRating, userId, mealTypeId) " +
+                "values (?,?,?,?,?,?,?,?,?,?,?);";
+
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, recipe.getRecipeId());
+            ps.setString(2, recipe.getRecipeName());
+            ps.setString(3, recipe.getRecipeStory());
+            ps.setString(4, recipe.getRecipeDescription());
+            ps.setString(5, recipe.getRecipeIngredients());
+            ps.setInt(6, recipe.getRecipeCookTime());
+            ps.setString(7, recipe.getRecipeSteps());
+            ps.setDate(8, Date.valueOf(recipe.getRecipeDate()));
+            ps.setDouble(9, recipe.getRecipeRating());
+            ps.setInt(10, recipe.getUserId());
+            ps.setInt(11, recipe.getMealTypeId());
+
+            return ps;
+        }, keyHolder);
+        if (rowsAffected <= 0) {
+            return null;
+        }
+        recipe.setRecipeId(keyHolder.getKey().intValue());
+        return recipe;
+    }
+
+    public boolean update(Recipe recipe) {
+        final String sql = "update recipe set " +
+                "recipeName = ?, " +
+                "recipeStory = ?, " +
+                "recipeDescription = ?, " +
+                "recipeIngredients = ?, " +
+                "recipeCookTime = ?, " +
+                "recipeCookTime = ?, " +
+                "recipeSteps = ?, " +
+                "recipeDate = ?, " +
+                "recipeRating = ?, " +
+                "userId = ?, " +
+                "mealTypeId = ? " +
+                "where recipeId = ?;";
+
+        return jdbcTemplate.update(sql,
+                recipe.getRecipeName(),
+                recipe.getRecipeStory(),
+                recipe.getRecipeDescription(),
+                recipe.getRecipeIngredients(),
+                recipe.getRecipeCookTime(),
+                recipe.getRecipeSteps(),
+                recipe.getRecipeDate(),
+                recipe.getRecipeRating(),
+                recipe.getUserId(),
+                recipe.getMealTypeId(),
+                recipe.getRecipeId()) > 0;
+    }
+
+    public boolean deleteById(int recipeId) {
+        jdbcTemplate.update("delete from recipeHealthInfo where recipeId = ?;", recipeId);
+        return jdbcTemplate.update("delete from recipe where recipeId = ?;", recipeId) > 0;
+    }
+
+
+//    private void addHealthInfo(Recipe recipe) {
+//
+//        final String sql = "select rhi.healthInfoId, rhi.recipeId "
+//                + "from recipeHealthInfo rhi  "
+//                + "where rhi.recipeId = ?;";
+//
+//        List<HealthInfoRecipe> healthInfo = jdbcTemplate.query(sql, new HealthInfoRecipe(), recipe.getRecipeId());
+//        recipe.setHealthInfo(healthInfo);
+//
+//    }
 }
--- server/src/main/java/learn/collaboreat/data/mappers/RecipeMapper.java
+++ server/src/main/java/learn/collaboreat/data/mappers/RecipeMapper.java
@@ -1,4 +1,34 @@
 package learn.collaboreat.data.mappers;
 
-public class RecipeMapper {
+import learn.collaboreat.models.MealType;
+import learn.collaboreat.models.Recipe;
+import org.springframework.jdbc.core.RowMapper;
+
+import javax.swing.tree.TreePath;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class RecipeMapper implements RowMapper<Recipe> {
+
+    @Override
+    public Recipe mapRow(ResultSet resultSet, int i) throws SQLException {
+        Recipe recipe = new Recipe();
+
+        recipe.setRecipeId(resultSet.getInt("recipeId"));
+        recipe.setRecipeName(resultSet.getString("recipeName"));
+        recipe.setRecipeStory(resultSet.getString("recipeStory"));
+        recipe.setRecipeDescription(resultSet.getString("recipeDescription"));
+        recipe.setRecipeIngredients(resultSet.getString("recipeIngredients"));
+        recipe.setRecipeCookTime(resultSet.getInt("recipeCookTime"));
+        recipe.setRecipeSteps(resultSet.getString("recipeSteps"));
+        recipe.setRecipeDate(resultSet.getDate("recipeDate").toLocalDate());
+        recipe.setRecipeRating(resultSet.getInt("recipeRating"));
+        recipe.setUserId(resultSet.getInt("userId"));
+        if (resultSet.getInt("mealTypeId") == 0) {
+            recipe.setMealTypeId(resultSet.getInt("mealTypeId"));
+        }
+
+        return recipe;
+    }
 }
