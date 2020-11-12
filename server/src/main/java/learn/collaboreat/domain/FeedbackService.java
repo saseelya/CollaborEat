844dd0f7ff16fa542328d66f2bf989000894a0e6
Nicholas Dorsett <ndorsett@dev-10.com>
2020-11-11 18:11:20 -0600

    feedbackService

--- server/src/main/java/learn/collaboreat/domain/FeedbackService.java
+++ server/src/main/java/learn/collaboreat/domain/FeedbackService.java
@@ -1,7 +1,66 @@
 package learn.collaboreat.domain;
 
+import learn.collaboreat.data.FeedbackJDBCTemplateRepository;
+import learn.collaboreat.models.Feedback;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class FeedbackService {
+
+    private FeedbackJDBCTemplateRepository repository;
+
+    public FeedbackService(FeedbackJDBCTemplateRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Feedback> findAll() {
+        return repository.findAll();
+    }
+
+    public Feedback findById(int feedbackId) {
+        return repository.findById(feedbackId);
+    }
+
+    public Result<Feedback> add(Feedback feedback) {
+        Result<Feedback> result = new Result<>();
+        if (feedback == null) {
+            result.addMessage("Feedback must exist.", ResultType.INVALID);
+            return result;
+        }
+
+        if (feedback.getFeedbackId() != 0) {
+            result.addMessage("Feedback ID must not be set in order to add.", ResultType.INVALID);
+            return result;
+        }
+
+        feedback = repository.add(feedback);
+        result.setPayload(feedback);
+        return result;
+    }
+
+    public Result<Feedback> update(Feedback feedback) {
+        Result<Feedback> result = new Result<>();
+        if (feedback == null) {
+            result.addMessage("Feedback must exist.", ResultType.INVALID);
+            return result;
+        }
+
+        if (feedback.getFeedbackId() <= 0) {
+            result.addMessage("Feedback ID must be set in order to update.", ResultType.INVALID);
+            return result;
+        }
+
+        if (!repository.update(feedback)) {
+            String msg = String.format("feedbackId: %s, not found.", feedback.getFeedbackId());
+            result.addMessage(msg, ResultType.NOT_FOUND);
+        }
+
+        return result;
+    }
+
+    public boolean deleteById(int feedbackId) {
+        return repository.deleteById(feedbackId);
+    }
 }
