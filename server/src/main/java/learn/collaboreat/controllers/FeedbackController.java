5c9c7f01843ab5bfbc8292a4406532440ef4e72a
jamie <jamielepito@gmail.com>
2020-11-12 17:06:51 -0600

    Build RecipeHealthInfo controller

--- server/src/main/java/learn/collaboreat/controllers/FeedbackController.java
+++ server/src/main/java/learn/collaboreat/controllers/FeedbackController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-// tehe
 @RestController
 @CrossOrigin(origins = {"localhost:3000"})
 @RequestMapping("/feedback")
---
+++ server/src/main/java/learn/collaboreat/controllers/RecipeHealthInfoController.java
@@ -0,0 +1,39 @@
+package learn.collaboreat.controllers;
+
+import learn.collaboreat.domain.RecipeService;
+import learn.collaboreat.domain.Result;
+import learn.collaboreat.models.HealthInfo;
+import learn.collaboreat.models.RecipeHealthInfo;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@CrossOrigin(origins = {"localhost:3000"})
+@RequestMapping("/recipe/healthInfo")
+public class RecipeHealthInfoController {
+
+    private final RecipeService service;
+
+    public RecipeHealthInfoController(RecipeService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> addHealthInfo(@RequestBody RecipeHealthInfo rhi) {
+        Result<Void> result = service.addHealthInfo(rhi);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/{recipeId}/{healthInfoId}")
+    public ResponseEntity<Object> deleteHealthInfoByKey(
+            @PathVariable int recipeId, @PathVariable int healthInfoId) {
+        if (service.deleteHealthInfoByKey(recipeId, healthInfoId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
