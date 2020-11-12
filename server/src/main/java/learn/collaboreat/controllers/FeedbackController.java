b62cb70b6ee129421e18a149a86b59b37b09f7d4
jamie <jamielepito@gmail.com>
2020-11-12 15:46:49 -0600

    Build Feedback Controller

---
+++ server/src/main/java/learn/collaboreat/controllers/FeedbackController.java
@@ -0,0 +1,62 @@
+package learn.collaboreat.controllers;
+
+import learn.collaboreat.domain.FeedbackService;
+import learn.collaboreat.domain.Result;
+import learn.collaboreat.models.Feedback;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = {"localhost:3000"})
+@RequestMapping("/feedback")
+public class FeedbackController {
+
+    private final FeedbackService service;
+
+    public FeedbackController(FeedbackService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public List<Feedback> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{feedbackId}")
+    public ResponseEntity<Object> findById(@PathVariable int feedbackId) {
+        Feedback feedback = service.findById(feedbackId);
+        if (feedback == null) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return ResponseEntity.ok(feedback);
+    }
+
+    @PostMapping
+    public ResponseEntity<Object> add(@RequestBody Feedback feedback) {
+        Result<Feedback> result = service.add(feedback);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @PutMapping
+    public ResponseEntity<Object> update(@RequestBody Feedback feedback) {
+        Result<Feedback> result = service.update(feedback);
+        if (result.isSuccess()) {
+            return new ResponseEntity<>(result.getPayload(), HttpStatus.CREATED);
+        }
+        return ErrorResponse.build(result);
+    }
+
+    @DeleteMapping("/{feedbackId}")
+    public ResponseEntity<Object> deleteById(@PathVariable int feedbackId) {
+        if (service.deleteById(feedbackId)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+}
