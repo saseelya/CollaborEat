89885315324d8d4b5bf71d32c7e285921d66e786
Cecilia <cbarkume@dev-10.com>
2020-11-17 21:20:20 -0600

    Finalize editRecipe and addRecipe forms

--- client/collaboreat-react/src/components/EditRecipe.js
+++ client/collaboreat-react/src/components/EditRecipe.js
@@ -129,67 +129,85 @@ export default function EditRecipe() {
       <h2>Edit a Recipe</h2>
       <Errors errors={errors} />
       <form onSubmit={handleEditSubmit}>
-      <input type="hidden" value={ recipe.recipeId }/>
-      <div>
-        <label htmlFor="recipeName">Recipe Name:  </label>
-        <input id="recipeName" value={recipeName} 
-          onChange={(event) => setRecipeName(event.target.value)} type="text" />
+
+      <div class="form-row">
+        <div class="form-group col-md-3">
+        <center><label htmlFor="recipeName">Recipe Name:</label></center>
+          <input className="form-control col-12" id="recipeName" value={recipeName} 
+            onChange={(event) => setRecipeName(event.target.value)} type="text" placeholder="Name that Recipe!" />
+        </div>
+
+        <div class="form-group col-md-3">
+        <center><label htmlFor="recipeCookTime">Cook Time:</label></center>
+          <input className="form-control col-12" id="recipeCookTime" value={recipeCookTime} 
+            onChange={(event) => setRecipeCookTime(event.target.value)} type="text" placeholder="How long does it take it make?" />
+        </div>  
+
+        <div class="form-group col-md-3">
+          <center><label htmlFor="mealType">Select a Meal Type:</label></center>
+          <select className="form-control col-12" id="mealType" value={mealTypeId} onChange={(event) => setMealTypeId(event.target.value)}>
+            <option value="1">Breakfast</option>
+            <option value="2">Brunch</option>
+            <option value="3">Desert</option>
+            <option value="4">Dinner</option>
+            <option value="5">Entree</option>
+            <option value="6">Lunch</option>
+            <option value="7">Side</option>
+            <option value="8">Snack</option>
+          </select>
+        </div>
+
+        <div class="form-group col-md-3">
+          <label>Select Health Info:  </label>
+          <Multiselect 
+            options={options}
+            selected={selected}
+            onSelect={(selected) => setSelected({selected})} // Function will trigger on select event
+            onRemove={(selected) => setSelected({selected})} // Function will trigger on remove event
+            displayValue="name" // Property name to display in the dropdown options
+            />
+        </div>
       </div>
-      <div>
-        <label htmlFor="recipeDescription">Recipe Description:  </label>
-        <input id="recipeDescription" value={recipeDescription} 
-          onChange={(event) => setRecipeDescription(event.target.value)} type="text"/>
+
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <center><label htmlFor="recipeDescription">Recipe Description:</label></center>
+          <textarea className="form-control col-16" id="recipeDescription" rows="8" value={recipeDescription} 
+            onChange={(event) => setRecipeDescription(event.target.value)} type="text" placeholder="Describe that recipe!" />
+        </div>
+        <div class="form-group col-md-6">
+          <center><label htmlFor="recipeStory">Recipe Story:  </label></center>
+          <textarea className="form-control col-16" rows="8" id="recipeStory" value={recipeStory} 
+            onChange={(event) => setRecipeStory(event.target.value)} type="text" placeholder="Tell other CollaborEaters about this recipe!" />
+        </div>
       </div>
-      <div>
-        <label htmlFor="recipeStory">Recipe Story:  </label>
-        <input id="recipeStory" value={recipeStory} 
-          onChange={(event) => setRecipeStory(event.target.value)} type="text" />
+
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <center><label htmlFor="recipeIngredients">Recipe Ingredients:  </label></center>
+          <textarea className="form-control col-16" rows="8" id="recipeIngredients" value={recipeIngredients} 
+            onChange={(event) => setRecipeIngredients(event.target.value)} type="text" placeholder="List your ingredients. Create a new line for each ingredient. Don't forget proportions!" />
+        </div>
+        <div class="form-group col-md-6">
+          <center><label htmlFor="recipeSteps">Recipe Steps:  </label></center>
+          <textarea className="form-control col-16" rows="8" id="recipeSteps" value={recipeSteps} 
+            onChange={(event) => setRecipeSteps(event.target.value)} type="text" placeholder="List your steps. Create a new line for each step. We'll take it from there." />
+        </div>
       </div>
-      <div>
-        <label htmlFor="recipeCookTime">Cook Time:  </label>
-        <input id="recipeCookTime" value={recipeCookTime} 
-          onChange={(event) => setRecipeCookTime(event.target.value)} type="text" />
+
+      <div class="form-row">
+        <div class="form-group col-md-10">
+          <center><label htmlFor="imageUrl">Link to Image:  </label></center>
+          <input className="form-control col-16" id="imageUrl" value={imageUrl} 
+            onChange={(event) => setImageUrl(event.target.value)} type="text" placeholder="Words can only do so much. Give the reader an idea of what the recipe looks like with an external link!" />
+        </div>
+
+        <div class="form-ground col-md-2">
+          <button className="btn btn-success form-control col-16" type="submit">Add Recipe</button>
+          <Link to={"/"}><button className="btn btn-danger form-control col-16">Cancel</button></Link>
+        </div>
       </div>
-      <div>
-        <label htmlFor="recipeIngredients">Recipe Ingredients:  </label>
-        <textarea id="recipeIngredients" value={recipeIngredients} 
-          onChange={(event) => setRecipeIngredients(event.target.value)} type="text"/>
-      </div>
-      <div>
-        <label htmlFor="recipeSteps">Recipe Steps:  </label>
-        <textarea id="recipeSteps" value={recipeSteps} type="text" 
-          onChange={(event) => setRecipeSteps(event.target.value)}/>
-      </div>
-      <div>
-        <label htmlFor="imageUrl">Link to Image:  </label>
-        <textarea id="imageUrl" value={imageUrl} 
-          onChange={(event) => setImageUrl(event.target.value)} type="text" />
-      </div>
-      <div>
-        <label htmlFor="mealType">Select a Meal Type:  </label>
-        <select id="mealType" value={mealTypeId} onChange={(event) => setMealTypeId(event.target.value)}>
-        <option value="1">Breakfast</option>
-          <option value="2">Brunch</option>
-          <option value="3">Desert</option>
-          <option value="4">Dinner</option>
-          <option value="5">Entree</option>
-          <option value="6">Lunch</option>
-          <option value="7">Side</option>
-          <option value="8">Snack</option>
-        </select>
-      </div>
-      <div>
-      <label>Select Health Info:  </label>
-      <Multiselect 
-        options={options}
-        selected={selected}
-        onSelect={(selected) => setSelected({selected})} // Function will trigger on select event
-        onRemove={(selected) => setSelected({selected})} // Function will trigger on remove event
-        displayValue="name" // Property name to display in the dropdown options
-        />
-      </div>
-      <button type="submit">Edit Recipe</button>
-      <Link to={"/"}>Cancel</Link>
+        
       </form>
     </>
   )
