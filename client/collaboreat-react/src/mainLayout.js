d5255d47197dd2b670e4425b9645fe7aab8c9ea4
Cecilia <cbarkume@dev-10.com>
2020-11-17 18:57:59 -0600

    Finalize homepage design (again)

--- client/collaboreat-react/src/mainLayout.js
+++ client/collaboreat-react/src/mainLayout.js
@@ -16,7 +16,7 @@ export default function MainLayout() {
                     src="cutting-tomatoes.png?text=First slide&bg=373940"
                     alt="First slide"
                     />
-                    <Carousel.Caption className="text-light my-custom-background">
+                    <Carousel.Caption className="text-dark silver-black-gradient">
                         <h3>Welcome to CollaborEat!</h3>
                         <p>On November 9th, 2020, three friends came together to create CollaborEat, a place for recipes of all kinds.</p>
                     </Carousel.Caption>
@@ -28,7 +28,7 @@ export default function MainLayout() {
                     alt="Second slide"
                     />
 
-                    <Carousel.Caption className="text-light my-custom-background">
+                    <Carousel.Caption className="text-dark silver-black-gradient">
                         <h3>Our Mission</h3>
                         <p>Here at CollaborEat, we believe that food has the unqiue power to change hearts and minds. Cooking is much more than combining ingredients.</p>
                     </Carousel.Caption>
@@ -40,7 +40,7 @@ export default function MainLayout() {
                     alt="Third slide"
                     />
 
-                    <Carousel.Caption className="text-light my-custom-background">
+                    <Carousel.Caption className="text-dark silver-black-gradient">
                         <h3>Interested in CollaborEat?</h3>
                         <p>Register today and start making memories, for free!</p>
                     </Carousel.Caption>
@@ -49,53 +49,53 @@ export default function MainLayout() {
 
             <div>
                 <Link to="/recipe/add">Add</Link>
-                <h1 className="text-center">Newest Submissions</h1>
+                <h1 className="text-center text-dark silver-black-gradient">Newest Submissions</h1>
                 <RecipeCardsByDate />
             </div>
             <div>
-            <h1 className="text-center">Top Breakfast Recipes</h1>
+            <h1 className="text-center  text-dark silver-black-gradient">Top Breakfast Recipes</h1>
                 <div>
                     <RecipeCardsByType mealTypeId={1}/>
                 </div>
             </div>
             <div>
-            <h1 className="text-center">Top Brunch Recipes</h1>
+            <h1 className="text-center text-dark silver-black-gradient">Top Brunch Recipes</h1>
                 <div>
                     <RecipeCardsByType mealTypeId={2}/>
                 </div>
             </div>
             <div>
-            <h1 className="text-center">Top Dessert Recipes</h1>
+            <h1 className="text-center text-dark silver-black-gradient">Top Dessert Recipes</h1>
                 <div>
                     <RecipeCardsByType mealTypeId={3}/>
                 </div>
             </div>
             <div>
-            <h1 className="text-center">Top Dinner Recipes</h1>
+            <h1 className="text-center text-dark silver-black-gradient">Top Dinner Recipes</h1>
                 <div>
                     <RecipeCardsByType mealTypeId={4}/>
                 </div>
             </div>
             <div>
-            <h1 className="text-center">Top Entree Recipes</h1>
+            <h1 className="text-center text-dark silver-black-gradient">Top Entree Recipes</h1>
                 <div>
                     <RecipeCardsByType mealTypeId={5}/>
                 </div>
             </div>
             <div>
-            <h1 className="text-center">Top Lunch Recipes</h1>
+            <h1 className="text-center text-dark silver-black-gradient">Top Lunch Recipes</h1>
                 <div>
                     <RecipeCardsByType mealTypeId={6}/>
                 </div>
             </div>
             <div>
-            <h1 className="text-center">Top Side Recipes</h1>
+            <h1 className="text-center text-dark silver-black-gradient">Top Side Recipes</h1>
                 <div>
                     <RecipeCardsByType mealTypeId={7}/>
                 </div>
             </div>
             <div>
-            <h1 className="text-center">Top Snack Recipes</h1>
+            <h1 className="text-center text-dark silver-black-gradient">Top Snack Recipes</h1>
                 <div>
                     <RecipeCardsByType mealTypeId={8}/>
                 </div>
